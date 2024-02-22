fd4b8a8b7c4c150291db6cb639c1a910fb9aaa28
Ashutosh Tiwari <97720235+InCoderWeb@users.noreply.github.com>
2024-02-22 23:36:46 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,51 @@
+"use client"
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { LuMenu, LuSearch } from "react-icons/lu";
+import { menuLinks } from "@/lib/Constants";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+	return (
+		<>
+			<section className="container flex justify-between relative items-center w-[98%] h-[4rem] my-2">
+				<aside className="flex justify-center items-center">
+					<img
+						src="./images/logo.png"
+						alt="Logo"
+						className="size-10 mr-2"
+					/>
+					<h1 className="text-2xl font-sans font-medium text-primary">
+						InCoder
+					</h1>
+				</aside>
+
+				<div className={`links absolute md:static w-[90%] md:w-fit top-[5rem] ${isOpen ? "block" : "hidden"}`}>
+					<ul className="flex md:space-x-6 w-full md:w-fit space-y-4 md:space-y-0 top-[2rem] justify-center items-center flex-col md:flex-row">
+						{menuLinks.map((l) => {
+							return (
+								<a
+									href={l.url}
+                  key={l.text}
+									className="inLink text-white/70 hover:text-white transition-colors"
+								>
+									<li>{l.text}</li>
+								</a>
+							);
+						})}
+					</ul>
+				</div>
+				<aside className="">
+					<Button className="w-min hover:text-primary !bg-transparent text-white">
+						<LuSearch className="size-6" />
+					</Button>
+					<Button onClick={() => setIsOpen(!isOpen)} className="md:hidden w-min hover:text-primary !bg-transparent text-white">
+						<LuMenu className="size-6" />
+					</Button>
+				</aside>
+			</section>
+		</>
+	);
+};
+
+export default Navbar;
---
+++ src/lib/Constants.tsx
@@ -0,0 +1,18 @@
+export const menuLinks = [
+	{
+		text: "HTML",
+		url: "/search/html/",
+	},
+	{
+		text: "CSS",
+		url: "/search/css/",
+	},
+	{
+		text: "Javascript",
+		url: "/search/javascript/",
+	},
+	{
+		text: "Projects",
+		url: "/search/projects/",
+	},
+];
